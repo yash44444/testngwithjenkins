dc0ab19b78d6042069bafa93dbad4069bc0dc483
yashu verma <yash01verma@gmail.com>
2022-12-26 00:56:57 +0000

    first comit

---
+++ maventest/src/test/java/packages/TestCase.java
@@ -0,0 +1,14 @@
+package packages;
+
+import org.testng.annotations.Test;
+
+public class TestCase {
+	
+	
+	@Test
+	public void sampleTest() {
+		System.out.println("hello world");
+		System.out.println("hello world!");
+	}
+
+}
